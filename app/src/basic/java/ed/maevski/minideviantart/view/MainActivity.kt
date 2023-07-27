7b3e3e2275be3983b55bfbfb5b5d81e6a5c08ad4
maevskiEd <maevski.ed@gmail.com>
2023-07-27 17:46:31 +0300

    feature_53. Finish. With star.

--- app/src/basic/java/ed/maevski/minideviantart/view/MainActivity.kt
+++ app/src/basic/java/ed/maevski/minideviantart/view/MainActivity.kt
@@ -4,12 +4,17 @@ import android.app.AlertDialog
 import android.content.BroadcastReceiver
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import ed.maevski.minideviantart.App
 import ed.maevski.minideviantart.R
 import ed.maevski.minideviantart.databinding.ActivityMainBinding
 import ed.maevski.minideviantart.receivers.ConnectionChecker
@@ -20,6 +25,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -43,6 +49,59 @@ class MainActivity : AppCompatActivity() {
             addAction(Intent.ACTION_BATTERY_LOW)
         }
         registerReceiver(receiver, filters)
+
+        if (!App.instance.isPromoShown) {
+            //Получаем доступ к Remote Config
+            val firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+            //Устанавливаем настройки
+            val configSettings = FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(0)
+                .build()
+            firebaseRemoteConfig.setConfigSettingsAsync(configSettings)
+            //Вызываем метод, который получит данные с сервера и вешаем слушатель
+            firebaseRemoteConfig.fetch()
+                .addOnCompleteListener {
+                    //Если все получилось успешно
+                    if (it.isSuccessful) {
+                        //активируем последний полученный конфиг с сервера
+                        firebaseRemoteConfig.activate()
+                        //Получаем ссылку
+                        val filmLink = firebaseRemoteConfig.getString("film_link")
+
+                        println("Ссылка на промо изображение: $filmLink")
+
+                        //Если поле не пустое
+                        if (filmLink.isNotBlank
+                                ()
+                        ) {
+                            //Ставим флаг, что уже промо показали
+                            App.instance.isPromoShown = true
+                            //Включаем промо верстку
+                            binding.promoViewGroup.apply {
+                                //Делаем видимой
+                                visibility = View.VISIBLE
+                                //Анимируем появление
+                                animate()
+                                    .setDuration(1500)
+                                    .alpha(1f)
+                                    .start()
+                                //Вызываем метод, который загрузит постер в ImageView
+                                setLinkForPoster(filmLink)
+                                //Кнопка, по нажатии на которую промо уберется (желательно сделать отдельную кнопку с крестиком)
+                                watchButton.setOnClickListener {
+                                    visibility = View.GONE
+                                    val uri = Uri.parse(filmLink)
+                                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                                    startActivity(intent)
+                                }
+
+                                //Кнопка, чтобы закрыть картинку
+                                closeButton.setOnClickListener { visibility = View.GONE }
+                            }
+                        }
+                    }
+                }
+        }
 
         scopeMainActivity = CoroutineScope(Dispatchers.IO).also { scope ->
             scope.launch {
